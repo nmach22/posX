57d525eba81f99edd440c0780a4ecf616c7f623b
alaki22 <alaki22@freeuni.edu.ge>
2025-02-25 19:38:18 +0400

    updated receipts

---
+++ app/core/Interfaces/receipt_repository_interface.py
@@ -0,0 +1,17 @@
+from typing import Protocol
+
+from app.core.Interfaces.receipt_interface import AddProductRequest, Receipt
+
+
+class ReceiptRepositoryInterface(Protocol):
+    def add_receipt(self, receipt: Receipt) -> Receipt:
+        pass
+
+    def close_receipt(self, receipt_id: str) -> None:
+        pass
+
+    def get_receipt(self, receipt_id: str) -> Receipt:
+        pass
+
+    def add_product_to_receipt(self, receipt_id: str, product_request: AddProductRequest) -> Receipt:
+        pass
