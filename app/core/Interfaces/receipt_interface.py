b0249b6d51637b13f4a61e8fcf7ce3bf2b203aed
alaki22 <alaki22@freeuni.edu.ge>
2025-02-25 16:00:27 +0400

    update pyproject

--- app/core/Interfaces/receipt_interface.py
+++ app/core/Interfaces/receipt_interface.py
@@ -1,9 +1,26 @@
+from dataclasses import dataclass
 from typing import Protocol
 
 from app.core.Interfaces.product_interface import Product
 
 
+@dataclass
+class Receipt:
+    id: str
+    products: list[Product]
+    status: str
+    total: int
+
 class ReceiptInterface(Protocol):
 
-    def add_product(self, product: Product) -> None:
+    def create_receipt(self)-> Receipt:
         pass
+
+    def add_product(self, product_id: str) -> Receipt:
+        pass
+
+    def close_receipt(self) -> None:
+        pass
+
+
+
