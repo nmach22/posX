141203c936f590d8a1b8ee40570a97c7d4f2e40a
zeliz22 <zeliz22@freeuni.edu.ge>
2025-02-25 18:37:18 +0400

    correct product_interface.py signature

--- app/core/Interfaces/product_interface.py
+++ app/core/Interfaces/product_interface.py
@@ -24,7 +24,7 @@ class ProductInterface(Protocol):
     def read_all_products(self) -> list[Product]:
         pass
 
-    def update_product_price(self, product: Product) -> None:
+    def update_product_price(self, product: Product) -> Product:
         pass
 
     def get_product(self, product_id: str) -> Product:
