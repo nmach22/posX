04208e7981bd2216416653cfd6033264f3c119dc
abarb2022 <abarb22@freeuni.edu.ge>
2025-02-25 22:03:36 +0400

    added shift logic

---
+++ app/core/Interfaces/shift_interface.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import Dict, Protocol
+
+from app.core.Interfaces.receipt_interface import Receipt
+
+
+@dataclass
+class Shift:
+    shift_id: str
+    receipts: list[Receipt]
+    status: str
+
+@dataclass
+class XReport:
+    shift_id: str
+    n_receipts: int
+    revenue: int
+    products: list[Dict[str, int]]
+
+
+class ShiftInterface (Protocol):
+    def create_shift(self) -> Shift:
+        pass
+
+    def close_shift(self, shift_id: str) -> None:
+        pass
+
+    def add_receipt_to_shift(self, receipt: Receipt, shift_id: str) -> None:
+        pass
+
+    def get_x_report(self, shift_id: str) -> XReport:
+        pass
+
