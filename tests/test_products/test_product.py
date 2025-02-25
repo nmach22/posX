bc12df6f87f95e55e89ad1ed583482e763a97cdf
zeliz22 <zeliz22@freeuni.edu.ge>
2025-02-25 13:09:01 +0400

    "added some functions in products"

--- tests/test_products/test_product.py
+++ tests/test_products/test_product.py
@@ -1,11 +1,22 @@
 from app.core.classes.product_service import ProductService
 from app.core.Interfaces.product_interface import Product, ProductRequest
 from app.infra.product_in_memory_repository import ProductInMemoryRepository
+import pytest
 
-
-def test_should_add_product_in_list() -> None:
+def test_should_add_product_in_memory() -> None:
     product_list : list[Product] = []
     service = ProductService(ProductInMemoryRepository(product_list))
     service.create_product(ProductRequest("lobio", 500, "123123"))
     assert len(product_list) == 1
     assert product_list[0].name == "lobio"
+
+
+def test_add_and_read_product() -> None:
+    product_list: list[Product] = []
+    service = ProductService(ProductInMemoryRepository(product_list))
+    product = service.create_product(ProductRequest("lobio", 500, "123123"))
+    returned_product = service.get_product(product.id)
+    assert returned_product.name == "lobio"
+    assert returned_product.price == 500
+    assert returned_product.price == 500
+
