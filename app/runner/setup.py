248cd4e8fb8fc55267611c9c94ebfef06a217823
nikamatcharadze <nmach22@freeuni.edu.ge>
2025-02-26 19:23:11 +0400

    post/campaigns endpoint

--- app/runner/setup.py
+++ app/runner/setup.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from app.infra.api.campaigns import campaigns_api
 from app.infra.api.products import products_api
 from app.infra.api.receipts import receipts_api
 from app.infra.in_memory import InMemory
