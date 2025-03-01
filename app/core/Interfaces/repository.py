3925a716f1d896a2ee428ad4edcbca5b9b131679
nikamatcharadze <nmach22@freeuni.edu.ge>
2025-03-01 22:11:01 +0400

    add repository interface

---
+++ app/core/Interfaces/repository.py
@@ -0,0 +1,17 @@
+from typing import TypeVar, Protocol
+
+ItemT = TypeVar("ItemT")
+
+
+class Repository(Protocol[ItemT]):
+    def create(self, item: ItemT) -> ItemT:
+        pass
+
+    def read(self, item_id: str) -> ItemT:
+        pass
+
+    def update(self, item: ItemT) -> None:
+        pass
+
+    def read_all(self) -> list[ItemT]:
+        pass
